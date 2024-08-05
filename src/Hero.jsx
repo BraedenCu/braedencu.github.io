b1ffe72019dbb2bb7fa67c95bb9b75b941e8abf7
Braeden Cullen <33903031+BraedenCu@users.noreply.github.com>
2024-08-05 00:09:25 -0500

    adjusted short bio

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -30,7 +30,7 @@ export default function Hero() {
           </div>
           <h4>{heading}</h4>
           <p>
-          I leverage software to address global challenges, with a strong foundation in AI, big data, and entrepreneurship. Engineering new experiences for future generations. Currently building at Elevance, the Yale Foreign Policy Institute (YFPI + DOD), and the Yale Entreprenurial Society (YES).
+          I leverage software to address global challenges, with a strong foundation in AI, big data, and entrepreneurship. Engineering new experiences for future generations. 
           </p>
         </div>
         <div className="menu">
